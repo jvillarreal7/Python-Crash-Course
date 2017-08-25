828cc46541532e34411b9c0be9eb0ab9044e3e71
jvillarreal7 <jvillarreal337@gmail.com>
2017-08-24 22:46:44 -0500

    Initial commit

---
+++ stripping_whitespaces.py
@@ -0,0 +1,6 @@
+favorite_language = 'python '
+print(favorite_language + '<---')
+print(favorite_language.rstrip() + '<---')
+
+favorite_language = ' python '
+print('--->' + favorite_language.strip() + '<---')
---
+++ whitespaces.py
@@ -0,0 +1,4 @@
+print("Normal: " + "Python")
+print("Tabbed: " + "\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
