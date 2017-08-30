449c2189f53880321a2a34c2ff8671695023317b
jvillarreal7 <jvillarreal337@gmail.com>
2017-08-30 00:50:54 -0500

    Typo fix.

--- 6 - Dictionaries/alien_speed2.py
+++ 6 - Dictionaries/alien_speed2.py
@@ -6,7 +6,7 @@ for alien_number in range(30):
 	new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
 	aliens.append(new_alien)
 
-# Show the first 5 aliens
+# Iterate and replace aliens
 for alien in aliens[:3]:
 	if alien['color'] == 'green':
 		alien['color'] = 'yellow'
